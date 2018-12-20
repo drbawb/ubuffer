a915cedc4a968ac5f5124a5c9def5cf62b86ee09
drbawb <drbawb@fatalsyntax.com>
2018-12-20 08:54:43 -0600

    improved cli interface
    - sender and receiver modes are now separate subcommands
    - there is a subcommand to generate a random key
    - key must now be provided by the user

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #[macro_use] extern crate log;
 #[macro_use] extern crate serde_derive;
 
+extern crate base64;
 extern crate bincode;
 extern crate byteorder;
 extern crate clap;
@@ -12,58 +13,108 @@ extern crate serde;
 extern crate udt;
 
 use crate::proto::{Sender, Receiver};
-
-use clap::{Arg, App};
+use clap::{Arg, App, SubCommand};
 use std::io;
 
 mod error;
 mod proto;
 
+const CLI_TITLE: &str = "UDT buffer"; 
+
+const CLI_SUB_GENKEY: &str = "genkey";
+const CLI_SUB_SEND: &str = "sender";
+const CLI_SUB_RECV: &str = "receiver";
+
+const CLI_ARG_KEY: &str = "KEY";
+const CLI_ARG_KEY_SHORT: &str = "k";
+const CLI_ARG_KEY_LONG: &str = "key";
+
+const CLI_TXT_APP: &str = "Transfer files between two nodes using the UDT protocol.";
+const CLI_TXT_KEY: &str = "The encryption key used to encrypt data blocks. (Must match on both sender & receiver.)";
+const CLI_TXT_GENKEY: &str = "generates a random encryption key on stdout (256-bits, base64 encoded)";
+const CLI_TXT_SEND: &str = "starts `ubuffer` in sender mode.";
+const CLI_TXT_RECV: &str = "starts `ubuffer` in receiver mode.";
+
 fn main() -> Result<(), failure::Error> {
 	env_logger::init();
 
-	let matches = App::new("UDT buffer")
+	let matches = App::new(CLI_TITLE)
 		.version(env!("CARGO_PKG_VERSION")) 
-		.about("Transfers files between two nodes using the UDT protocol.")
-		.arg(Arg::with_name("recv_addr")
-			 .short("r")
-			 .long("recv")
-			 .value_name("RECV_ADDR")
-			 .takes_value(true)
-			 .help("Starts listening on specified inet address for incoming data."))
-		.arg(Arg::with_name("send_addr")
-			 .short("s")
-			 .long("send")
-			 .value_name("SEND_ADDR")
-			 .takes_value(true)
-			 .help("Sends stdin to the specified receiver."))
+		.about(CLI_TXT_APP)
+		.subcommand(SubCommand::with_name(CLI_SUB_GENKEY)
+					.about(CLI_TXT_GENKEY))
+		.subcommand(SubCommand::with_name(CLI_SUB_SEND)
+					.about(CLI_TXT_SEND)
+					.arg(Arg::with_name("REMOTE_ADDR")
+						 .help("The address of the remote peer waiting in `receiver` mode.")
+						 .required(true))
+					.arg(Arg::with_name(CLI_ARG_KEY)
+						 .short(CLI_ARG_KEY_SHORT)
+						 .long(CLI_ARG_KEY_LONG)
+						 .help(CLI_TXT_KEY)
+						 .required(true)))
+		.subcommand(SubCommand::with_name(CLI_SUB_RECV)
+					.about(CLI_TXT_RECV)
+					.arg(Arg::with_name("LISTEN_ADDR")
+						 .help("The address and port to listen on for incoming senders.")
+						 .required(true))
+					.arg(Arg::with_name(CLI_ARG_KEY)
+						 .short(CLI_ARG_KEY_SHORT)
+						 .long(CLI_ARG_KEY_LONG)
+						 .help(CLI_TXT_KEY)))
 		.get_matches();
 
-	if matches.is_present("send_addr") {
-		let addr = matches.value_of("send_addr")
-			.expect("Entered sending mode w/o supplying an address?");
+	if let Some(_cmd) = matches.subcommand_matches("sender") {
+		let key = matches.value_of(CLI_ARG_KEY)
+			.expect("fatal: sender requires an encryption key.");
 
-		start_sender(addr)?;
-	} else if matches.is_present("recv_addr") {
-		let addr = matches.value_of("recv_addr")
-			.expect("Entered receiver mode w/o supplying an address?");
+		let addr = matches.value_of("REMOTE_ADDR")
+			.expect("fatal: sender requires a remote address.");
 
-		start_receiver(addr)?;
+		start_sender(addr, key)?;
+	} else if let Some(_cmd) = matches.subcommand_matches("receiver") {
+		let key = matches.value_of(CLI_ARG_KEY)
+			.expect("fatal: sender requires an encryption key.");
+
+		let addr = matches.value_of("LISTEN_ADDR")
+			.expect("fatal: sender requires a remote address.");
+
+		start_receiver(addr, key)?;
+	} else if let Some(_cmd) = matches.subcommand_matches("genkey") {
+		genkey();
+	} else {
+		panic!("no matching subcommand specified?");
 	}
 
 	Ok(())
 }
 
-fn start_sender(addr: &str) -> Result<(), failure::Error> {
-	let mut sender = Sender::new(addr, b"dcb23a241cd82c4152b8e5637a4ae121")?;
+fn start_sender(addr: &str, key: &str) -> Result<(), failure::Error> {
+	let key = base64::decode(key)?;
+	let mut sender = Sender::new(addr, &key)?;
 	sender.run(io::stdin())?;
 
 	Ok(())
 }
 
-fn start_receiver(addr: &str) -> Result<(), failure::Error> {
-	let mut receiver = Receiver::new(addr, b"dcb23a241cd82c4152b8e5637a4ae121")?;
+fn start_receiver(addr: &str, key: &str) -> Result<(), failure::Error> {
+	let key = base64::decode(key)?;
+	let mut receiver = Receiver::new(addr, &key)?;
 	receiver.run(io::stdout())?;
 
 	Ok(())
+}
+
+fn genkey() {
+	use rand::Rng;
+
+	let mut rng = rand::thread_rng();
+	let mut key = [0u8; 32];
+
+	for key_byte in &mut key {
+		*key_byte = rng.gen();
+	}
+
+	let key_b64 = base64::encode(&key);
+	println!("{}", key_b64);
 }
